55d1984948f981b6cc6241e6a6b1bbdfa12918f1
Vinod Mendis <136561979+Vinod-Mendis@users.noreply.github.com>
2025-01-06 13:16:03 +0700

    import components to main page

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,11 @@
 
 import { useState } from "react";
 import Sidebar from "./components/Sidebar";
+import Navbar from "./components/navbar/NavBar";
+import Modal from "./components/modal/Modal";
 
 export default function Home() {
-  const [selectedExercise, setSelectedExercise] = useState('Navbar');
+  const [selectedExercise, setSelectedExercise] = useState("Navbar");
 
   const handleSelect = (exercise) => {
     setSelectedExercise(exercise);
@@ -14,9 +16,13 @@ export default function Home() {
   return (
     <div className="w-screen h-screen p-5 flex gap-5">
       <Sidebar onSelectFunction={handleSelect} />
-      <div className="w-full h-full rounded-xl border shadow-xl p-4">
+      <div className="w-full h-full rounded-xl border shadow-xl  p-4">
         <h1 className="text-4xl mb-3 font-semibold">{selectedExercise}.</h1>
         <hr />
+        <div className="w-full h-5/6 mt-10 p-20 bg-slate-50 rounded-xl flex justify-center items-center ">
+          {selectedExercise === "Navbar" && <Navbar />}
+          {selectedExercise === "Modal" && <Modal />}
+        </div>
       </div>
     </div>
   );
